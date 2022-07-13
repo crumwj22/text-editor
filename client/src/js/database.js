ab0a707116fc2613b73d4cd749e60cb2a81c4598
crumwj22 <crumwj22@hotmail.com>
2022-07-12 23:30:13 -0700

    modified syntax in files to match database syntax

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,10 +15,10 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error("putDb not implemented");
-  const todosDb = await openDB("todos", 1);
-  const tx = todosDb.transaction("todos", "readwrite");
-  const store = tx.objectStore("todos");
-  const request = store.put({ id: id, todo: content });
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
